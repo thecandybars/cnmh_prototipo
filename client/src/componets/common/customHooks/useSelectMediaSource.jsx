cc2355ae30f638997ea4402eaa4c09f4df35b0ce
Andrés Vilá <vilaandres@gmail.com>
2024-08-28 14:12:03 -0500

    Refactored useSelectedImage to include selectMediaSource funcionality

--- client/src/componets/common/customHooks/useSelectMediaSource.jsx
+++ client/src/componets/common/customHooks/useSelectMediaSource.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
-import selectMediaSource from "../../../utils/selectMediaSource";
+import axios from "axios";
+import getEnv from "../../../utils/getEnv";
 
 export default function useSelectMediaSource(sources) {
   const [source, setSource] = useState("");
   useEffect(() => {
     const check = async () => {
-      const source = await selectMediaSource({
-        primary: sources.primary,
-        secondary: sources.secondary,
+      const mediaUrl = getEnv("media");
+      const ipfsGateway = getEnv("pinataGateway");
+      const response = await axios.head(`${mediaUrl}/${sources.primary}`, {
+        validateStatus: () => true,
       });
+      const source =
+        response.status >= 200 && response.status < 300
+          ? `${mediaUrl}/${sources.primary}`
+          : `${ipfsGateway}/${sources.secondary}`;
       setSource(source);
     };
     check();
