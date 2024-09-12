79aea1f2fad58a18f5adc9d259ce858f54846dd5
Andrés Vilá <vilaandres@gmail.com>
2024-09-12 16:57:02 -0500

    Created Multimedia2: Vercel is complaining about Multimedia.jsx

--- client/src/componets/Exhibiciones/Multimedia2.jsx
+++ client/src/componets/Exhibiciones/Multimedia2.jsx
@@ -3,12 +3,12 @@ import useFetch from "../common/customHooks/useFetch";
 import { getExhibicion } from "../../services/exhibiciones";
 import MultimediaExhibicion from "./MultimediaComponents/MultimediaExhibicion";
 
-Multimedia.propTypes = {
+Multimedia2.propTypes = {
   exhibicionId: PropTypes.number,
   onClose: PropTypes.func,
 };
 //
-export default function Multimedia(props) {
+export default function Multimedia2(props) {
   const [exhibicion] = useFetch(() => getExhibicion(props.exhibicionId));
   return (
     exhibicion &&
