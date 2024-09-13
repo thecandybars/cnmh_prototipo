60f60204b99bfbe157f12d5ae6b05a6dffa109be
Andrés Vilá <vilaandres@gmail.com>
2024-09-13 12:03:59 -0500

    Vercel deploy Could not resolve Multimedia2.jsx - created new file MultimediaSliders.jsx

--- client/src/componets/Exhibiciones/MultimediaSliders.jsx
+++ client/src/componets/Exhibiciones/MultimediaSliders.jsx
@@ -3,12 +3,12 @@ import useFetch from "../common/customHooks/useFetch";
 import { getExhibicion } from "../../services/exhibiciones";
 import MultimediaExhibicion from "./MultimediaComponents/MultimediaExhibicion";
 
-Multimedia2.propTypes = {
+MultimediaSliders.propTypes = {
   exhibicionId: PropTypes.number,
   onClose: PropTypes.func,
 };
 //
-export default function Multimedia2(props) {
+export default function MultimediaSliders(props) {
   const [exhibicion] = useFetch(() => getExhibicion(props.exhibicionId));
   return (
     exhibicion &&
