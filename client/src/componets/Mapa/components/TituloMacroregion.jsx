cc6422684b1e1bf5bf6a7938b5617d628a1523b7
Andrés Vilá <vilaandres@gmail.com>
2024-09-18 09:59:31 -0500

    Refactored TituloMacroregion.jsx

--- client/src/componets/Mapa/components/TituloMacroregion.jsx
+++ client/src/componets/Mapa/components/TituloMacroregion.jsx
@@ -3,60 +3,56 @@ import { theme } from "../../../utils/theme";
 import PropTypes from "prop-types";
 
 TituloMacroregion.propTypes = {
-  label: PropTypes.string,
   title: PropTypes.string,
-  display: PropTypes.bool,
 };
 
 export default function TituloMacroregion(props) {
   const yellowLinesStyle = `3px solid ${theme.palette.secondary.main}`;
   return (
-    props.title && (
-      <Grid
-        container
-        sx={{
-          position: "absolute",
-          color: "primary.main",
-          bottom: 0,
-          left: 0,
-          width: "25%",
-          zIndex: 1,
-          display: "flex",
-        }}
-      >
-        <Grid item xs={2} sx={{ borderRight: yellowLinesStyle }}></Grid>
-        <Grid item xs={10}>
-          <Typography variant="h5" pt={4} pl={1}>
-            {props.label || "REGIÓN"}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={2}
-          sx={{
-            borderRight: yellowLinesStyle,
-            borderBottom: yellowLinesStyle,
-          }}
-        ></Grid>
-        <Grid item xs={10} sx={{ borderBottom: yellowLinesStyle }}>
-          <Typography
-            variant="h1"
-            sx={{
-              mb: -4,
-              pl: 1,
-              textShadow: `2px 2px 4px ${theme.palette.translucidBlack}`,
-            }}
-          >
-            {props.title}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={2}
-          sx={{ borderRight: yellowLinesStyle, height: "40px" }}
-        ></Grid>
-        <Grid item xs={10}></Grid>
+    <Grid
+      container
+      sx={{
+        position: "absolute",
+        color: "primary.main",
+        bottom: 0,
+        left: 0,
+        width: "25%",
+        zIndex: 1,
+        display: "flex",
+      }}
+    >
+      <Grid item xs={2} sx={{ borderRight: yellowLinesStyle }}></Grid>
+      <Grid item xs={10}>
+        <Typography variant="h5" pt={4} pl={1}>
+          {props.title ? "REGIÓN" : "COLOMBIA"}
+        </Typography>
       </Grid>
-    )
+      <Grid
+        item
+        xs={2}
+        sx={{
+          borderRight: yellowLinesStyle,
+          borderBottom: yellowLinesStyle,
+        }}
+      ></Grid>
+      <Grid item xs={10} sx={{ borderBottom: yellowLinesStyle }}>
+        <Typography
+          variant="h1"
+          sx={{
+            mb: -4,
+            pl: 1,
+            textShadow: `2px 2px 4px ${theme.palette.translucidBlack}`,
+          }}
+        >
+          {props.title || "Macroregiones"}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={2}
+        sx={{ borderRight: yellowLinesStyle, height: "40px" }}
+      ></Grid>
+      <Grid item xs={10}></Grid>
+    </Grid>
   );
 }
