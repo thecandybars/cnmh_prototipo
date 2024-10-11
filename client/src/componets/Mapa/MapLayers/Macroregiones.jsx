e59e3ec454fba570309caa3de19d7e93a7f3f193
Andrés Vilá <vilaandres@gmail.com>
2024-10-11 18:45:03 -0500

    Quitar hover en Macroregion activa

--- client/src/componets/Mapa/MapLayers/Macroregiones.jsx
+++ client/src/componets/Mapa/MapLayers/Macroregiones.jsx
@@ -1,55 +1,55 @@
 import { Layer, Source } from "react-map-gl";
-import colombiaRegionsData from "../../../geojson/col.json";
-import useFetch from "../../common/customHooks/useFetch";
-import { getAllDepartamentos } from "../../../services/departamentos";
-import { theme } from "../../../utils/theme";
+import colombiaRegionsData from "../../../geojson/macroregiones.json";
 import useAppStore from "../../../store/useAppStore";
 
 export default function Macroregiones(props) {
-  const actualView = useAppStore((state) => state.actualView);
   const actualRegion = useAppStore((state) => state.actualRegion);
+  console.log("🚀 ~ Macroregiones ~ actualRegion:", actualRegion);
+  const actualView = useAppStore((state) => state.actualView);
 
-  const [departamentos] = useFetch(() => getAllDepartamentos());
+  const DrawRegions = colombiaRegionsData.features.map((feature) => {
+    console.log("🚀 ~ DrawRegions ~ feature:", feature);
 
-  const DrawRegions =
-    departamentos?.length &&
-    colombiaRegionsData.features.map((feature) => {
-      const id = parseInt(feature.properties.dpto);
-      const region = departamentos?.find((dpto) => dpto.geoId === id)?.Region;
-      const regionColor = {
-        color: theme.palette[region.name]?.first || "gray",
-        opacity: 0.3,
-      };
-      if (actualView !== 0 && region?.id === actualRegion?.id)
-        regionColor.opacity = 0.1;
-      if (props.hoverFeature?.id === `zone-${id}-fill`)
-        regionColor.color = "pink";
-      return (
-        <Source
-          key={`zone-${id}`}
-          id={`zone-${id}`}
-          type="geojson"
-          data={feature}
-        >
-          <Layer
-            onMouseEnter={() => console.log(id)}
-            id={`zone-${id}-fill`}
-            type="fill"
-            paint={{
-              "fill-color": regionColor.color,
-              "fill-opacity": regionColor.opacity,
-            }}
-          />
-          <Layer
-            id={`zone-${id}-line`}
-            type="line"
-            paint={{
-              "line-color": "rgba(90, 125, 0, 0.18)",
-              "line-width": 1,
-            }}
-          />
-        </Source>
-      );
-    });
+    const id = parseInt(feature.properties.id);
+    const isHover = props.hoverFeature?.id === `macroregion-${id}`;
+    const type = "fill";
+    const calcOpacity = () => {
+      if (actualView === 0) {
+        return isHover ? 0.2 : 0;
+      }
+      if (actualView === 1) {
+        return isHover && actualRegion?.id !== Number(feature.properties.id)
+          ? 0.2
+          : 0;
+      }
+    };
+    const paint = {
+      "fill-color": "#cacaca",
+      "fill-opacity": calcOpacity(),
+    };
+
+    return (
+      <Source
+        key={`macroregion-${id}`}
+        id={`macroregion-${id}`}
+        type="geojson"
+        data={feature}
+      >
+        {/* 3D Extruded Layer */}
+        <Layer id={`macroregion-${id}`} type={type} paint={paint} />
+        {/* Line Layer */}
+        <Layer
+          id={`macroregion-${id}-line`}
+          type="line"
+          paint={{
+            "line-color": "#cacaca",
+            "line-width": 3,
+            "line-opacity": 0.5,
+          }}
+        />
+      </Source>
+    );
+  });
+
   return DrawRegions;
 }
