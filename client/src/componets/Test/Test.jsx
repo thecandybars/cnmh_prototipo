a8af59f36c10909a717e5e60423881626baf3cb8
Andrés Vilá <vilaandres@gmail.com>
2024-10-22 14:35:37 -0500

    Primer commit de prueba para modulo de renderizado 3D

--- client/src/componets/Test/Test.jsx
+++ client/src/componets/Test/Test.jsx
@@ -1,21 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 export default function Test() {
+  const canvasRef = useRef();
   useEffect(() => {
-    const checkIpfs = () => {
-      if (window.ipfs) {
-        console.log("IPFS Companion is running");
-      } else {
-        console.log("IPFS Companion is not running or not installed.");
-      }
-    };
+    // Scene
+    const scene = new THREE.Scene();
+    // Sphere
+    const geometry = new THREE.SphereGeometry(1, 64, 64);
+    const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 3;
+    scene.add(camera);
+    // Light
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }, [canvasRef]);
 
-    // Wait a bit for IPFS Companion to initialize
-    const intervalId = setInterval(checkIpfs, 1000); // Check every second
-
-    // Clean up interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, []); // Empty dependency array ensures it runs only on mount
-
-  return <div>Test</div>;
+  return (
+    <div>
+      Test
+      <canvas ref={canvasRef} style={{ border: "1px solid red" }} />
+    </div>
+  );
 }
