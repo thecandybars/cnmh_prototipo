b895d12e103346e5e8b0bafa84bbf3dd62d4a68b
Andrés Vilá <vilaandres@gmail.com>
2024-10-31 19:20:23 -0500

    Prueba de VideoScroll para Siloe

--- client/src/componets/Lugares/useScrollyVideo.jsx
+++ client/src/componets/Lugares/useScrollyVideo.jsx
@@ -2,24 +2,38 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import ScrollyVideo from "scrolly-video/dist/ScrollyVideo.esm.jsx";
 
-function useScrollyVideo({ src, length }) {
+function useScrollyVideo({ src = "", length = 300 }) {
   const [scrollyPosition, setScrollyPosition] = useState(0);
-  const calcHeight = (heightPercent) => {
-    return (length * (1 - 10 / length) - 200) * -heightPercent + "vh";
-  };
+
   const Element = ({ children }) => (
-    <div className={"scrolly-container"} style={{ height: `${length}vh` }}>
-      <ScrollyVideo src={src} onChange={(e) => setScrollyPosition(e)} />
+    <div className="scrolly-container" style={{ height: `${length}vh` }}>
+      <ScrollyVideo
+        src={src}
+        onChange={(position) => setScrollyPosition(position)}
+      />
       {children}
     </div>
   );
+
+  Element.propTypes = {
+    children: PropTypes.node,
+  };
 
   return [Element, calcHeight, scrollyPosition];
 }
 
 useScrollyVideo.propTypes = {
   src: PropTypes.string,
-  length: PropTypes.number,
+  length: PropTypes.number.isRequired,
+};
+
+/**
+ * Given a height percentage, calculate the equivalent height in viewport units.
+ * @param {number} heightPercent - A number from 0 to 1, representing the percentage of viewport height.
+ * @returns {string} The equivalent height in viewport units.
+ */
+const calcHeight = (heightPercent) => {
+  return `${(length * (1 - 10 / length) - 200) * -heightPercent}vh`;
 };
 
 export default useScrollyVideo;
--- client/src/componets/Test/Scrolly.jsx
+++ client/src/componets/Test/Scrolly.jsx
@@ -46,56 +46,163 @@
 
 import { useState } from "react";
 import ScrollyVideo from "scrolly-video/dist/ScrollyVideo.esm.jsx";
-import { Fade } from "@mui/material";
+import { Box, Dialog, Fade, Typography } from "@mui/material";
+import video from "/siloe.mp4";
 
 function Scrolly() {
   const [scrollyPosition, setScrollyPosition] = useState(0);
-  console.log("🚀 ~ Scrolly ~ scrollyPosition:", scrollyPosition);
-  const altura = 400;
+  const [openDialog, setOpenDialog] = useState(false);
+  const [showHiddenText, setShowHiddenText] = useState(false);
+  const altura = 800;
   const calcAltura = (perc) => {
     return (altura * (1 - 10 / altura) - 200) * -perc + "vh";
+    // return altura * -perc + "vh";
   };
   return (
     <div>
-      <h1 style={{ marginBottom: "200px" }}>ScrollyVideo Test</h1>
-      <h1 style={{ marginBottom: "200px" }}>ScrollyVideo Test</h1>
-      <h1 style={{ marginBottom: "200px" }}>ScrollyVideo Test</h1>
-      <h1 style={{ marginBottom: "200px" }}>ScrollyVideo Test</h1>
-      <h1 style={{ marginBottom: "200px" }}>ScrollyVideo Test</h1>
+      <Typography variant="h3" m={4}>
+        Video Scroll de Siloé
+      </Typography>
+
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <Box width={400} height={400} p={2}>
+          Esta es una ventana de dialogo. Los dialogos bloquean la interacción
+          con el resto del contenido e impiden hacer scroll.
+          <b> ESC o click afuera para cerrar</b>
+        </Box>
+      </Dialog>
+
       <div className={"scrolly-container"} style={{ height: `${altura}vh` }}>
         <ScrollyVideo
-          src="https://scrollyvideo.js.org/goldengate.mp4"
+          src={video}
+          // src="https://scrollyvideo.js.org/goldengate.mp4"
           onChange={(e) => setScrollyPosition(e)}
+          // cover={false}
         />
-        <p style={{ position: "relative", bottom: calcAltura(0) }}>Hola!</p>
-        <p
+        <div
           style={{
-            position: "relative",
-            bottom: calcAltura(0.3),
-            left: "100px",
+            width: "400px",
+            margin: "20px",
           }}
         >
-          Hola hacia un lado!
-        </p>
-        <p style={{ position: "relative", bottom: calcAltura(0.5) }}>
-          ...en el medio
-        </p>
-        <p
-          style={{ position: "relative", bottom: calcAltura(1), left: "200px" }}
+          <Typography
+            variant="h4"
+            style={{
+              position: "relative",
+              bottom: calcAltura(0),
+              color: "red",
+            }}
+          >
+            Esto es un titulo
+          </Typography>
+
+          <p
+            variant="body"
+            style={{
+              position: "relative",
+              bottom: calcAltura(0),
+              color: "white",
+              backgroundColor: "rgba(0,0,0,0.5)",
+              padding: "10px",
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipiscing elit justo, augue
+            imperdiet molestie rutrum aliquet pharetra suscipit venenatis
+            hendrerit, pellentesque donec fringilla lacus orci magna euismod.
+            Ridiculus blandit non varius bibendum duis montes lacus, integer
+            fringilla enim lectus natoque aliquam, egestas eleifend pellentesque
+            id netus tristique.
+          </p>
+        </div>
+        <h1
+          style={{
+            position: "relative",
+            bottom: calcAltura(0.5),
+            color: "white",
+            margin: "20px",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            padding: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => setOpenDialog(true)}
         >
-          Hola final!
-        </p>
-        <Fade in={scrollyPosition > 0.3 && scrollyPosition < 0.5} timeout={300}>
-          <h2 style={{ position: "fixed", top: "200px", left: "400px" }}>
-            Soy FIXED
+          Click para abrir dialogo
+        </h1>
+        <h1
+          style={{
+            position: "relative",
+            bottom: calcAltura(0.7),
+            color: "white",
+            margin: "20px",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            padding: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => setShowHiddenText((prev) => !prev)}
+        >
+          Pero se pueden hacer links que abren y cierran cajas de texto que
+          fluyen con el scroll. Haz click.
+        </h1>
+        {showHiddenText && (
+          <p
+            style={{
+              position: "relative",
+              bottom: calcAltura(0.7),
+              color: "white",
+              margin: "20px",
+              backgroundColor: "rgba(175, 210, 108, 0.5)",
+              padding: "10px",
+              width: "300px",
+            }}
+          >
+            Algo asi! Hendrerit laoreet cursus id conubia diam in egestas, et
+            quam non mi facilisis dignissim eu ad, curabitur semper pretium
+            blandit ridiculus imperdiet. Ac iaculis quis sed dictumst libero
+            neque pretium, vehicula natoque praesent proin primis ante vulputate
+            ornare, at a facilisis tempus ridiculus nibh.
+          </p>
+        )}
+        <h1
+          style={{
+            position: "relative",
+            bottom: "-530vh",
+            // bottom: calcAltura(1),
+            color: "white",
+            margin: "20px",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            padding: "10px",
+          }}
+        >
+          Este es el último texto
+        </h1>
+        <Fade
+          in={scrollyPosition > 0.27 && scrollyPosition < 0.39}
+          timeout={700}
+        >
+          <h2
+            style={{
+              position: "fixed",
+              top: "400px",
+              left: "400px",
+              color: "red",
+              width: "300px",
+              backgroundColor: "rgba(255,255,255,0.5)",
+              padding: "10px",
+            }}
+          >
+            Este texto aparece fijado a la pantalla y sincronizado con la
+            entrada y salida del niño de saco rojo
           </h2>
         </Fade>
       </div>
-      <h3 style={{ margin: "200px 0" }}>Ultimas palabras</h3>
-      <h3 style={{ margin: "200px 0" }}>Ultimas palabras</h3>
-      <h3 style={{ margin: "200px 0" }}>Ultimas palabras</h3>
-      <h3 style={{ margin: "200px 0" }}>Ultimas palabras</h3>
-      <h3 style={{ margin: "200px 0" }}>Ultimas palabras</h3>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
+      <h1 style={{ margin: "40px" }}>La pagina sigue con su flujo normal</h1>
     </div>
   );
 }
