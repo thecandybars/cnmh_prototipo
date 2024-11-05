e5e233080f605f11b3e7ec5cdc8c2b147d6a1d32
Andrés Vilá <vilaandres@gmail.com>
2024-11-05 12:07:30 -0500

    Error al desplegar por problemas de nombres de archivo

--- client/src/componets/common/InteractiveImage.jsx
+++ client/src/componets/common/InteractiveImage.jsx
@@ -1,50 +1,10 @@
 import PropTypes from "prop-types";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import mural from "/MuralBocachico.jpg";
 import { useState } from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-const Zoom = () => {
-  const hotspots = [
-    {
-      id: "pescao",
-      top: "70%",
-      left: "60%",
-      content:
-        "Bibendum eleifend id mattis auctor in duis ad aliquet, praesent facilisi maecenas mollis arcu congue fames penatibus tellus, porttitor commodo tincidunt rutrum interdum habitasse ornare. Duis porta euismod sagittis montes facilisis pellentesque aliquam dis habitant, a interdum congue maecenas fames malesuada metus sed, quam magna donec tincidunt dignissim leo ullamcorper nec.",
-    },
-    {
-      id: "camion",
-      top: "40%",
-      left: "20%",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit accumsan habitasse at netus fermentum felis lacus ad donec, nec tristique ultrices habitant natoque curae aliquet maecenas iaculis etiam commodo magna dapibus platea nascetur. Rutrum iaculis felis nam aliquet congue primis risus nunc dapibus, dis netus potenti feugiat purus mauris interdum pulvinar aptent scelerisque, est egestas varius fringilla in libero tellus viverra.",
-    },
-    {
-      id: "grupo",
-      top: "45%",
-      left: "51%",
-      content:
-        "Ridiculus tristique vulputate neque eleifend justo tempus integer, mauris per vel purus nisi suscipit, fusce pharetra malesuada rhoncus natoque interdum. Posuere cras ullamcorper laoreet consequat neque curae enim semper hac lobortis dictumst donec vestibulum aliquam proin dapibus, libero nulla et rutrum ultrices cum nam vitae duis quis porttitor ac morbi convallis",
-    },
-  ];
 
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <InteractiveImage src={mural} hotspots={hotspots} zoom={3.5} />
-    </div>
-  );
-};
-
-const InteractiveImage = ({ src, hotspots = [], zoom }) => {
+export default function InteractiveImage({ src, hotspots = [], zoom }) {
   const [showInfo, setShowInfo] = useState({ visible: false, content: "" });
   const [resetZoom, setResetZoom] = useState(null);
 
@@ -133,7 +93,7 @@ const InteractiveImage = ({ src, hotspots = [], zoom }) => {
       )}
     </div>
   );
-};
+}
 
 InteractiveImage.propTypes = {
   src: PropTypes.string.isRequired,
@@ -147,5 +107,3 @@ InteractiveImage.propTypes = {
   ),
   zoom: PropTypes.number.isRequired,
 };
-
-export default Zoom;
